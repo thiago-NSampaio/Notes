d6dd3a5b531619a567ead2e29792fb4cdae1a72e
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2024-01-14 14:24:18 -0400

    Autenticando o usuário na aplicação

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,9 +3,22 @@ import { FiMail, FiLock } from "react-icons/fi";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/auth";
+import { useState } from "react";
 
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const { signIn } = useAuth();
+  
+  function handleSignIn(e) {
+    e.preventDefault()
+    signIn({email, password});
+  }
+
   return(
     <Container>
       <Form>
@@ -14,12 +27,12 @@ export function SignIn() {
 
         <h2>Faça seu login</h2>
 
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input placeholder="E-mail" type="text" icon={FiMail} onChange={ e => setEmail(e.target.value)} />
               
 
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input placeholder="Senha" type="password" icon={FiLock} onChange={ e => setPassword(e.target.value)}/>
               
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={ handleSignIn} />
         <Link to="/register">
           Criar conta
         </Link>
